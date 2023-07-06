ac8e954e39c61d8a8e06fa845705c643e46d8d7c
Vishwa Navod <giuvnavod@gmail.com>
2023-07-06 10:44:33 +0530

    Initial Commit

---
+++ app/src/main/java/com/vu/assessment_4669025_4669026/AddCarActivity.java
@@ -0,0 +1,4 @@
+package com.vu.assessment_4669025_4669026;
+
+public class AddCarActivity {
+}
---
+++ app/src/main/java/com/vu/assessment_4669025_4669026/MainActivity.java
@@ -0,0 +1,15 @@
+package com.vu.assessment_4669025_4669026;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
