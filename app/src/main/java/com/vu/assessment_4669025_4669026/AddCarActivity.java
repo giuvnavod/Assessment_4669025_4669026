7a09810145598508053eccaec9e49079e3537640
Vishwa Navod <giuvnavod@gmail.com>
2023-07-06 11:08:21 +0530

    created java files

--- app/src/main/java/com/vu/assessment_4669025_4669026/AddCarActivity.java
+++ app/src/main/java/com/vu/assessment_4669025_4669026/AddCarActivity.java
@@ -1,4 +1,13 @@
 package com.vu.assessment_4669025_4669026;
 
-public class AddCarActivity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddCarActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_addcar);
+    }
 }
