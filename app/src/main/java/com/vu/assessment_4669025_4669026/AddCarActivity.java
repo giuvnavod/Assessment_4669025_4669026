f222363f4d187f821ab324ecd302c001de4676ec
Vishwa Navod <giuvnavod@gmail.com>
2023-07-06 14:34:41 +0530

    created xml files , java files , buttons

--- app/src/main/java/com/vu/assessment_4669025_4669026/AddCarActivity.java
+++ app/src/main/java/com/vu/assessment_4669025_4669026/AddCarActivity.java
@@ -1,13 +1,54 @@
 package com.vu.assessment_4669025_4669026;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class AddCarActivity extends AppCompatActivity {
+public class AddCarActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnAddCar,btnBack;
+    EditText edCarBrand,edCarModel,edCarPrice;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_addcar);
+
+        btnAddCar= (Button) findViewById(R.id.btnAddCar);
+        btnBack= (Button) findViewById(R.id.btnBack);
+
+        edCarBrand=(EditText) findViewById(edCarBrand)
+        edCarModel=(EditText) findViewById(edCarModel)
+        edCarPrice=(EditText) findViewById(edCarPrice)
+
+        btnAddCar.setOnClickListener(this);
+        btnBack.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id= v.getId();
+        if (id==R.id.btnAddCar){
+            addCar();
+        }
+        if (id==R.id.btnBack){
+            goIntoMainPage();
+        }
+    }
+
+    private void goIntoMainPage() {
+        Intent intent=new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    private void addCar() {
+        String brand = edCarBrand.getText().toString();
+        String model = edCarModel.getText().toString();
+        String price = edCarPrice.getText().toString();
+
+        boolean
     }
 }
--- app/src/main/java/com/vu/assessment_4669025_4669026/MainActivity.java
+++ app/src/main/java/com/vu/assessment_4669025_4669026/MainActivity.java
@@ -2,14 +2,45 @@ package com.vu.assessment_4669025_4669026;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnAddCar,btnCheckCar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnAddCar = (Button) findViewById(R.id.button);
+        btnCheckCar = (Button) findViewById(R.id.button2);
+
+        btnAddCar.setOnClickListener(this);
+        btnCheckCar.setOnClickListener(this);
     }
 
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        if (id == R.id.button){
+            goToAddCar();
+        }
+        if (id == R.id.button2){
+            goToCheckCar();
+        }
+    }
+
+    private void goToCheckCar() {
+        Intent intent = new Intent(this,CheckCarActivity.class);
+        startActivity(intent);
+    }
+
+    private void goToAddCar() {
+        Intent intent = new Intent(this,AddCarActivity.class);
+        startActivity(intent);
+    }
 }
