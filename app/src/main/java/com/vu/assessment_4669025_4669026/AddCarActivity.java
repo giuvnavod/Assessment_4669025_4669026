5d1a29b46def5eb05dcd5e20be4df0b141079c7f
Vishwa Navod <giuvnavod@gmail.com>
2023-07-06 16:22:03 +0530

    Change the code to identify if any field is empty

--- app/src/main/java/com/vu/assessment_4669025_4669026/AddCarActivity.java
+++ app/src/main/java/com/vu/assessment_4669025_4669026/AddCarActivity.java
@@ -5,13 +5,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.vu.assessment_4669025_4669026.data.DBHelper;
 
 public class AddCarActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button btnAddCar,btnBack;
-    EditText edCarBrand,edCarModel,edCarPrice;
+    EditText etCarBrand,etCarModel,etCarPrice;
+
+    DBHelper databasehelper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,12 +25,14 @@ public class AddCarActivity extends AppCompatActivity implements View.OnClickLis
         btnAddCar= (Button) findViewById(R.id.btnAddCar);
         btnBack= (Button) findViewById(R.id.btnBack);
 
-        edCarBrand=(EditText) findViewById(edCarBrand)
-        edCarModel=(EditText) findViewById(edCarModel)
-        edCarPrice=(EditText) findViewById(edCarPrice)
+        etCarBrand= findViewById(R.id.etCarBrand);
+        etCarModel= findViewById(R.id.etCarModel);
+        etCarPrice= findViewById(R.id.etCarPrice);
 
         btnAddCar.setOnClickListener(this);
         btnBack.setOnClickListener(this);
+
+        databasehelper = new DBHelper(this);
     }
 
     @Override
@@ -45,10 +52,23 @@ public class AddCarActivity extends AppCompatActivity implements View.OnClickLis
     }
 
     private void addCar() {
-        String brand = edCarBrand.getText().toString();
-        String model = edCarModel.getText().toString();
-        String price = edCarPrice.getText().toString();
+        String brand = etCarBrand.getText().toString();
+        String model = etCarModel.getText().toString();
+        String price = etCarPrice.getText().toString();
 
-        boolean
+        boolean isInserted = databasehelper.addCar(brand, model, Integer.parseInt(price));
+
+        if((brand.isEmpty() || model.isEmpty() || price.isEmpty()) && isInserted) {
+            Toast.makeText(AddCarActivity.this, "Enter values to all inputs", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(AddCarActivity.this, "Data inserted successfully", Toast.LENGTH_LONG).show();
+        }
+
+      /* boolean isInserted = databasehelper.addCar(brand,model,Integer.parseInt(price));
+        if (isInserted){
+            Toast.makeText(AddCarActivity.this,"Your Data Inserted Successfully",Toast.LENGTH_LONG).show();
+        }else {
+            Toast.makeText(AddCarActivity.this,"Your Data Insertion Failed",Toast.LENGTH_LONG).show();
+        }*/
     }
 }
